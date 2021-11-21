43f30fe5bfd0b72d2526647266c2b8e4042c9bb2
ianlaurindotolotti <ianlaurindo45@gmail.com>
2021-11-21 02:28:21 -0300

    Books created

--- server.js
+++ server.js
@@ -7,8 +7,13 @@ if (process.env.NODE_ENV !== 'production') {
 const express = require("express")
 const app = express()
 const expressLayouts = require("express-ejs-layouts")
+const bodyParser = require("body-parser")
 
 const indexRouter = require('./routes/index')
+const authorRouter = require('./routes/authors')
+const bookRouter = require('./routes/books')
+
+
 
 
 app.set("view engine", "ejs")
@@ -16,6 +21,7 @@ app.set("views", __dirname + "/views")
 app.set("layout", "layouts/layout")
 app.use(expressLayouts)
 app.use(express.static("public"))
+app.use(bodyParser.urlencoded({limit: '10mb', extended: false}))
 
 const mongoose = require("mongoose")
 
@@ -27,5 +33,8 @@ db.once('open', () => console.log('Connected to Mongoose'))
 
 
 app.use('/', indexRouter)
+app.use('/authors', authorRouter)
+app.use('/books', bookRouter)
 
-app.listen(process.env.PORT || 8723)
+
+app.listen(process.env.PORT || 3403)
