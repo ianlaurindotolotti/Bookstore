6a8dbd0045b89cb73a74a1ccc24c53d500a7b852
ianlaurindotolotti <ianlaurindo45@gmail.com>
2021-11-21 18:55:23 -0300

    File UploadDatabase

--- routes/index.js
+++ routes/index.js
@@ -4,7 +4,7 @@ const Book = require('../models/book')
 
 router.get('/', async (req, res) => {
 
-    let books
+    let books  
     try {
         books = await Book.find().sort({createAt: 'desc'}).limit(10).exec()
     } catch {
